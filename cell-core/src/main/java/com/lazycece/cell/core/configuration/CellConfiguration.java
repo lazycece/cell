93f3598b8e684569724680c627d17c8122561868
lazycece <lazycece@gmail.com>
2023-10-21 00:32:09 +0800

    CellConfiguration

---
+++ cell-core/src/main/java/com/lazycece/cell/core/configuration/CellConfiguration.java
@@ -0,0 +1,104 @@
+/*
+ *    Copyright 2023 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.cell.core.configuration;
+
+import com.lazycece.cell.core.model.spec.CellType;
+
+/**
+ * @author lazycece
+ * @date 2023/10/20
+ */
+public class CellConfiguration {
+
+    /**
+     * The cell type class
+     */
+    private Class<? extends CellType> cellTypeClass;
+
+    /**
+     * The data center
+     */
+    private Integer dataCenter = 0;
+
+    /**
+     * The machine
+     */
+    private Integer machine = 0;
+
+    /**
+     * min value
+     */
+    private Integer minValue;
+
+    /**
+     * max value
+     */
+    private Integer maxValue;
+
+    /**
+     * the step, that the interval size of the value.
+     */
+    private Integer step;
+
+    public Class<? extends CellType> getCellTypeClass() {
+        return cellTypeClass;
+    }
+
+    public void setCellTypeClass(Class<? extends CellType> cellTypeClass) {
+        this.cellTypeClass = cellTypeClass;
+    }
+
+    public Integer getDataCenter() {
+        return dataCenter;
+    }
+
+    public void setDataCenter(Integer dataCenter) {
+        this.dataCenter = dataCenter;
+    }
+
+    public Integer getMachine() {
+        return machine;
+    }
+
+    public void setMachine(Integer machine) {
+        this.machine = machine;
+    }
+
+    public Integer getMinValue() {
+        return minValue;
+    }
+
+    public void setMinValue(Integer minValue) {
+        this.minValue = minValue;
+    }
+
+    public Integer getMaxValue() {
+        return maxValue;
+    }
+
+    public void setMaxValue(Integer maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    public Integer getStep() {
+        return step;
+    }
+
+    public void setStep(Integer step) {
+        this.step = step;
+    }
+}
