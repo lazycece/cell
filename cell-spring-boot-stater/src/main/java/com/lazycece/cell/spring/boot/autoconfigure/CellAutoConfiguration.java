ec3172420936e233b0402f1e9aed7497d49938ed
lazycece <lazycece@gmail.com>
2023-11-05 01:15:43 +0800

    stater

---
+++ cell-spring-boot-stater/src/main/java/com/lazycece/cell/spring/boot/autoconfigure/CellAutoConfiguration.java
@@ -0,0 +1,101 @@
+/*
+ *    Copyright 2023 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.cell.spring.boot.autoconfigure;
+
+import com.lazycece.cell.core.buffer.CellBufferManager;
+import com.lazycece.cell.core.configuration.BufferConfiguration;
+import com.lazycece.cell.core.exception.CellAssert;
+import com.lazycece.cell.specification.CellFacade;
+import com.lazycece.cell.specification.configuration.CellConfiguration;
+import com.lazycece.cell.specification.impl.CellFacadeImpl;
+import com.lazycece.cell.specification.model.CellSpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lazycece
+ * @date 2023/10/31
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.lazycece.cell"})
+@EnableConfigurationProperties({CellProperties.class})
+@AutoConfigureBefore(value = {CellFacade.class, CellBufferManager.class})
+public class CellAutoConfiguration implements BeanPostProcessor, InitializingBean {
+
+    private final Logger log = LoggerFactory.getLogger(CellAutoConfiguration.class);
+    private BufferConfiguration bufferConfiguration;
+    @Autowired
+    private CellProperties cellProperties;
+
+    /**
+     * @see InitializingBean#afterPropertiesSet()
+     */
+    @Override
+    public void afterPropertiesSet() {
+        // check specification
+        CellConfiguration spec = cellProperties.getSpecification();
+        CellAssert.notNull(spec.getCellTypeClass(), "Cell specification check: cellTypeClass is null");
+        CellAssert.isTrue(spec.getDataCenter() < Math.pow(10, CellSpec.CELL_DATA_CENTER_LEN), "Cell specification check: dataCenter length limit %s", CellSpec.CELL_DATA_CENTER_LEN);
+        CellAssert.isTrue(spec.getDataCenter() < Math.pow(10, CellSpec.CELL_MACHINE_LEN), "Cell specification check: machine length limit %s", CellSpec.CELL_MACHINE_LEN);
+        CellAssert.isTrue(spec.getMinValue() < spec.getMaxValue(), "Cell specification check: limit minValue<maxValue");
+        CellAssert.isTrue(spec.getStep() < spec.getMaxValue() && spec.getStep() > spec.getMinValue(), "Cell specification check: limit minValue<step<maxValue");
+
+        // assemble buffer configuration
+        CellProperties.CellBufferProperties buffer = cellProperties.getBuffer();
+        int multiple = (int) Math.pow(2, buffer.getExpansionStepElasticityTime());
+        bufferConfiguration = new BufferConfiguration();
+        bufferConfiguration.setExpansionThreshold(buffer.getExpansionThreshold());
+        bufferConfiguration.setExpansionInterval(buffer.getExpansionInterval());
+        bufferConfiguration.setExpansionMinStep(spec.getStep() / multiple);
+        bufferConfiguration.setExpansionMaxStep(spec.getStep() * multiple);
+        bufferConfiguration.setThreadPoolCoreSize(buffer.getThreadPoolCoreSize());
+        bufferConfiguration.setThreadPoolMaxSize(buffer.getThreadPoolMaxSize());
+        bufferConfiguration.setThreadPoolKeepAliveTime(buffer.getThreadPoolKeepAliveTime());
+    }
+
+    /**
+     * @see BeanPostProcessor#postProcessAfterInitialization
+     */
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        cellFacadeInterceptor(bean);
+        cellBufferManagerInterceptor(bean);
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+
+    private void cellFacadeInterceptor(Object bean) {
+        if (bean instanceof CellFacadeImpl cellFacade) {
+            cellFacade.setConfiguration(cellProperties.getSpecification());
+            log.info("Cell auto configuration: set cell specification configuration.");
+        }
+    }
+
+    private void cellBufferManagerInterceptor(Object bean) {
+        if (bean instanceof CellBufferManager cellBufferManager) {
+            cellBufferManager.setBufferConfig(bufferConfiguration);
+            log.info("Cell auto configuration: set cell buffer configuration.");
+        }
+    }
+}
---
+++ cell-spring-boot-stater/src/main/java/com/lazycece/cell/spring/boot/autoconfigure/CellProperties.java
@@ -0,0 +1,134 @@
+/*
+ *    Copyright 2023 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.cell.spring.boot.autoconfigure;
+
+import com.lazycece.cell.specification.configuration.CellConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author lazycece
+ * @date 2023/11/4
+ */
+@ConfigurationProperties(prefix = "cell")
+public class CellProperties {
+
+    private CellConfiguration specification;
+    private CellBufferProperties buffer;
+
+    public CellConfiguration getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(CellConfiguration specification) {
+        this.specification = specification;
+    }
+
+    public CellBufferProperties getBuffer() {
+        return buffer;
+    }
+
+    public void setBuffer(CellBufferProperties buffer) {
+        this.buffer = buffer;
+    }
+
+    public static class CellBufferProperties {
+
+        /**
+         * Cell buffer expansion threshold, default value is 0.75
+         */
+        private double expansionThreshold = 0.75;
+
+        /**
+         * Cell buffer expansion elasticity time. If value is m,
+         * indicates the step interval is [step/(2^m),step*(2^m)]
+         */
+        private int expansionStepElasticityTime = 2;
+
+        /**
+         * Cell buffer refresh interval time (milliseconds)
+         * <p>
+         * Compute by qps and default step, for example
+         * <li>condition: step=6000, qps=10</li>
+         * <li>then: interval=step/qps=600s </li>
+         * </p>
+         */
+        private long expansionInterval = 10 * 60 * 1000;
+
+        /**
+         * Cell buffer thread pool core size, default value is 5
+         */
+        private int threadPoolCoreSize = 5;
+
+        /**
+         * Cell buffer thread pool max size, default value is ${@code Integer.MAX_VALUE}
+         */
+        private int threadPoolMaxSize = Integer.MAX_VALUE;
+
+        /**
+         * Cell buffer thread pool keep alive time, default value is 60s
+         */
+        private long threadPoolKeepAliveTime = 60L;
+
+        public double getExpansionThreshold() {
+            return expansionThreshold;
+        }
+
+        public void setExpansionThreshold(double expansionThreshold) {
+            this.expansionThreshold = expansionThreshold;
+        }
+
+        public int getExpansionStepElasticityTime() {
+            return expansionStepElasticityTime;
+        }
+
+        public void setExpansionStepElasticityTime(int expansionStepElasticityTime) {
+            this.expansionStepElasticityTime = expansionStepElasticityTime;
+        }
+
+        public long getExpansionInterval() {
+            return expansionInterval;
+        }
+
+        public void setExpansionInterval(long expansionInterval) {
+            this.expansionInterval = expansionInterval;
+        }
+
+        public int getThreadPoolCoreSize() {
+            return threadPoolCoreSize;
+        }
+
+        public void setThreadPoolCoreSize(int threadPoolCoreSize) {
+            this.threadPoolCoreSize = threadPoolCoreSize;
+        }
+
+        public int getThreadPoolMaxSize() {
+            return threadPoolMaxSize;
+        }
+
+        public void setThreadPoolMaxSize(int threadPoolMaxSize) {
+            this.threadPoolMaxSize = threadPoolMaxSize;
+        }
+
+        public long getThreadPoolKeepAliveTime() {
+            return threadPoolKeepAliveTime;
+        }
+
+        public void setThreadPoolKeepAliveTime(long threadPoolKeepAliveTime) {
+            this.threadPoolKeepAliveTime = threadPoolKeepAliveTime;
+        }
+    }
+}
