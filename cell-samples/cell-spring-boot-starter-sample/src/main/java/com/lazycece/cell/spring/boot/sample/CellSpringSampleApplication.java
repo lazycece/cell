ac6b059b17b54b8d7a05791bcd9a0703b9742bbc
lazycece <lazycece@gmail.com>
2023-11-08 20:38:07 +0800

    mdf

--- cell-samples/cell-spring-boot-starter-sample/src/main/java/com/lazycece/cell/spring/boot/sample/CellSpringSampleApplication.java
+++ cell-samples/cell-spring-boot-starter-sample/src/main/java/com/lazycece/cell/spring/boot/sample/CellSpringSampleApplication.java
@@ -16,13 +16,9 @@
 
 package com.lazycece.cell.spring.boot.sample;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@MapperScan(basePackages = {"com.lazycece.cell"})
-@ComponentScan(basePackages = {"com.lazycece.cell"})
 @SpringBootApplication
 public class CellSpringSampleApplication {
 
