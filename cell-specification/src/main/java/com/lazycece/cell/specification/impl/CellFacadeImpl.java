e0131a50caa6e2cbfcb27363c65244c7ee217d7a
lazycece <lazycece@gmail.com>
2023-11-10 00:35:12 +0800

    cfi

--- cell-specification/src/main/java/com/lazycece/cell/specification/impl/CellFacadeImpl.java
+++ cell-specification/src/main/java/com/lazycece/cell/specification/impl/CellFacadeImpl.java
@@ -18,18 +18,19 @@ package com.lazycece.cell.specification.impl;
 
 import com.lazycece.cell.core.buffer.CellBufferManager;
 import com.lazycece.cell.core.exception.CellAssert;
-import com.lazycece.cell.specification.configuration.CellConfiguration;
 import com.lazycece.cell.core.infra.repository.CellRegistryRepository;
 import com.lazycece.cell.core.model.CellRegistry;
 import com.lazycece.cell.specification.CellFacade;
+import com.lazycece.cell.specification.configuration.CellSpecConfiguration;
 import com.lazycece.cell.specification.factory.CellRegistryFactory;
 import com.lazycece.cell.specification.model.Cell;
 import com.lazycece.cell.specification.model.CellBuilder;
 import com.lazycece.cell.specification.model.CellType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
@@ -45,10 +46,11 @@ import java.util.Date;
  * @date 2023/9/8
  */
 @Component
-public class CellFacadeImpl implements CellFacade, InitializingBean {
+public class CellFacadeImpl implements CellFacade, ApplicationListener<ContextRefreshedEvent> {
 
     private final Logger log = LoggerFactory.getLogger(CellFacadeImpl.class);
-    private CellConfiguration configuration = new CellConfiguration();
+    private CellSpecConfiguration configuration = new CellSpecConfiguration();
+    private Class<? extends CellType> cellTypeClass;
     @Autowired
     private CellRegistryRepository cellRegistryRepository;
     @Autowired
@@ -73,25 +75,26 @@ public class CellFacadeImpl implements CellFacade, InitializingBean {
     }
 
     /**
-     * @see InitializingBean#afterPropertiesSet
+     * @see ApplicationListener#onApplicationEvent
      */
     @Override
-    public void afterPropertiesSet() {
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         boolean exist = cellRegistryRepository.existCellRegistry();
         CellAssert.isTrue(exist, "The expected table(cell_registry) not exist in db.");
-        initCellRegistry();
+        autoRegisterCell();
         CellBufferManager.getInstance().initCache();
         log.info("Cell started successfully.");
     }
 
     /**
-     * init cell registry to db.
+     * Register cell to db.
      */
-    private void initCellRegistry() {
-        Class<? extends CellType> cellTypeClass = configuration.getCellTypeClass();
-        CellAssert.notNull(cellTypeClass, "Cell configuration (cell type class) not exist.");
-        CellAssert.isTrue(cellTypeClass.isEnum(), "Cell configuration (cell type class) not enum.");
-
+    private void autoRegisterCell() {
+        if (cellTypeClass == null) {
+            log.info("Cell type class is null, not be registered automatically.");
+            return;
+        }
+        CellAssert.isTrue(cellTypeClass.isEnum(), "Cell type class not enum.");
         Arrays.asList(cellTypeClass.getEnumConstants())
                 .forEach(cellType ->
                         transactionTemplate.execute(new TransactionCallbackWithoutResult() {
@@ -107,7 +110,12 @@ public class CellFacadeImpl implements CellFacade, InitializingBean {
                 );
     }
 
-    public void setConfiguration(CellConfiguration configuration) {
+    public void setCellTypeClass(Class<? extends CellType> cellTypeClass) {
+        this.cellTypeClass = cellTypeClass;
+    }
+
+    public void setConfiguration(CellSpecConfiguration configuration) {
         this.configuration = configuration;
     }
+
 }
